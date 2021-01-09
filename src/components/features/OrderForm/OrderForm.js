ba21d1d103de8e0ba43c76854a7cfb971dbab737
Paulina Babinczuk <paulina.babinczuk@gmail.com>
2021-01-09 18:42:30 +0100

    calculate total price

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Col, Row } from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
+import OrderSummary from '../OrderSummary/OrderSummary';
+
+
+const OrderForm = ({ tripCost, options }) => (
+  <Row>
+    <Col xs={12}>
+      <OrderSummary tripCost={tripCost} options={options} />
+    </Col>
+  </Row>
+);
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.any,
+};
+
+
+export default OrderForm;
